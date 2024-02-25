3f5c7f268408725ecf38346c3dd2f75eacd7e790
Vinos Sarhad <vinos.sarhadd@gmail.com>
2024-02-25 21:04:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+// function saturdayFun( argument="roller-skate"){
+//     console.log("This Saturday, I want to  ! "+ argument);
+// }
+// saturdayFun("bathe my dog")
+// saturdayFun( )
+
+
+
+// Define the saturdayFun function with a default activity
+function saturdayFun(activity = 'roller-skate') {
+   return "This Saturday, I want to "+  activity +"!";
+  }
+  
+//   // Export the saturdayFun function
+//   module.exports = { saturdayFun };
+
+function mondayWork( activity="go to the office"){
+    return "This Monday, I will "+ activity+"."
+
+}
+
+function wrapAdjective(symbol='*'){
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+      };
+
+
+}
+console.log(wrapAdjective())
--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,7 @@ require ( './helpers.js' );
 
 describe("index.js", () => {
   describe("defines saturdayFun function declaration as specified", function() {
-    it("function exists", function() {
+    it("function exists", function() { 
       expect(saturdayFun).to.exist
     })
 
